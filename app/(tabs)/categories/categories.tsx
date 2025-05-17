375f30741cba29e28cd8703c9bd0a1f366ce7f2a
Samantha Alvarenga <samantha.c.alvarenga@gmail.com>
2025-05-16 22:31:44 -0300

    terminado categoryItem e criado a tela categories e populando a lista com flatList

--- app/(tabs)/categories/categories.tsx
+++ app/(tabs)/categories/categories.tsx
@@ -1,15 +1,29 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
 import CategoriesItem from "../../../components/CategoryItem/categoriesitem";
+import { getAllCatergories } from "../../../services/category";
+
+
+
+
 
 export default function categories() {
 
+    const categories = getAllCatergories();
     return (
 
         <View>
-            <Text>Sou a Categories</Text>
-           <CategoriesItem title={"Informática"} /> 
-           <CategoriesItem title={"Informática"} /> 
-           <CategoriesItem title={"Informática"} /> 
+            {/*     {categories.map((category) => (
+                <Text>{`ID: ${category} - NOME: ${category}`}</Text> 
+
+               <CategoriesItem category={category} />
+            ))}*/}
+
+            <FlatList
+                data={categories} //informando a fonte de dados da flatlist
+                renderItem={({ item }) => <CategoriesItem category={item} />} //informando a função que renderizará cada item, estamos usando uma arrow function no caso 
+                keyExtractor={id => id.id.toString()} //identificação do index de cada item.
+            >
+            </FlatList>
         </View>
     );
 
--- components/CategoryItem/categoriesitem.tsx
+++ components/CategoryItem/categoriesitem.tsx
@@ -1,36 +1,58 @@
-import { TouchableOpacity, Text, Image, StyleSheet, View } from "react-native";
-
+import { TouchableOpacity, Text, Image, StyleSheet, View, Pressable } from "react-native";
+import { Category } from "../../types/category";
+import {router} from 'expo-router';
 
 type Props = {
-    title: string,
-
+    category: Category
 }
 
 
 
-export default function categoriesItem(prop: Props) {
+export default function categoriesItem({category}: Props) {
+
+    function handleClick() {
+        router.push(`/categories/${category.id}`)
+    }
+
     return (
 
-       
-            <TouchableOpacity  style={styles.container}>
-                <Image resizeMode="cover" style={styles.img} source={require('./../../assets/image/0-1000x300.jpg')} />
+        < TouchableOpacity onPress={handleClick} style={styles.container} >
+            <Image resizeMode="cover" style={styles.img} source={{uri: category.cover}} />
 
-                <Text style={styles.buttonImg}>{prop.title}</Text>
-            </TouchableOpacity>
-      
+            <Text style={styles.buttonText}>{category.title}</Text>
+        </TouchableOpacity >
+
     )
 }
 
 
 const styles = StyleSheet.create({
     container: {
-        alignItems: 'center'
+        alignItems: 'center',
+        justifyContent: 'center',
+        alignSelf: 'center',
+        width: '80%',
+        height: 150,
+        margin: 15,
+        backgroundColor: '#000',
+        overflow: 'hidden',
+        borderRadius: 20,
     },
 
     img: {
-        width: '80%',
-        height: 150
-    }
+        width: '100%',
+        height: '100%',
+        margin: 15,
+        opacity: 0.3
+
+    },
+
+    buttonText: {
+        position: 'absolute',
+        color: '#fff',
+        fontSize: 25,
+
+    },
 
 
 });
